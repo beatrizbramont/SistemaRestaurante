cfa2959e89fb6d4c2300f9c677601e897febb7bf
Wender1903 <wenderman74@gmail.com>
2025-08-18 20:39:03 -0300

    :sparkles: feat: Configurando Banco de Dados MySql de local para online no railway para todos do grupo poderem acessar o bd em tempo real (não sei se existe algum limite de tempo pra usar esse Bando de Dados online, tem que ver)

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,7 +7,7 @@ app.config['HOST'] = '0.0.0.0'
 app.config['PORT']= 8001 
 app.config['DEBUG'] = True
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1903@localhost/cardapio"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:ZTGLRCLQDkLxeFadnWNbLUoLNhhbkLfU@caboose.proxy.rlwy.net:17980/railway"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
