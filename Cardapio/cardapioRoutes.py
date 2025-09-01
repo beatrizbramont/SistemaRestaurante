7281887af99f45119996858a5ce1bcabdef8a887
beatrizbramont <beatriz.bramont@gmail.com>
2025-09-01 10:30:22 -0300

    fix: atualizando rotas para puxar a responsividade corretamente

--- Cardapio/cardapioRoutes.py
+++ Cardapio/cardapioRoutes.py
@@ -6,12 +6,14 @@ from Cardapio.cardapioModels import db, Cardapio
 
 cardapio_bp = Blueprint("cardapio", __name__)
 frontend_bp = Blueprint('frontend', __name__)
-
 STATIC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'frontend')
 
 @frontend_bp.route('/')
 def serve_frontend():
     return send_from_directory(STATIC_DIR, 'index.html')
+@frontend_bp.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(STATIC_DIR, filename)
 
 @cardapio_bp.route("/cardapio", methods=["POST"])
 def create_item():
