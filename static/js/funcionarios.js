c5c63337069591b8c60480259d6d2875b29ba046
Wender1903 <wenderman74@gmail.com>
2025-10-27 18:42:22 -0300

    :sparkles: feat: Adicionando Interatividade de preview de imagem no cadastro de funcionário

--- static/js/funcionarios.js
+++ static/js/funcionarios.js
@@ -21,3 +21,28 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const input = document.getElementById("imagem");
+    const fileName = document.getElementById("file-name");
+    const preview = document.getElementById("preview-img");
+
+    if (input) {
+        input.addEventListener("change", function () {
+            const file = this.files[0];
+
+            if (file) {
+                fileName.textContent = file.name;
+
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    preview.src = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            } else {
+                fileName.textContent = "Nenhum arquivo selecionado";
+                preview.src = "https://cdn-icons-png.flaticon.com/512/66/66779.png";
+            }
+        });
+    }
+});
