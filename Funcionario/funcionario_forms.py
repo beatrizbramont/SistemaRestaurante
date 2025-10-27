ff1e0ada13c66b711ec73f733937a61bb92a3534
Wender1903 <wenderman74@gmail.com>
2025-10-27 18:43:58 -0300

    :sparkles: feat: Adicionando forms para cadastro de funcionario com o flaskForms

--- Funcionario/funcionario_forms.py
+++ Funcionario/funcionario_forms.py
@@ -1,9 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, SelectField, FileField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_wtf.file import FileAllowed
 
 class CadastroFuncionarioForm(FlaskForm):
-    ...
+    nome = StringField('Nome', validators=[DataRequired(), Length(min=4, max=120)])
+    cargo = SelectField('Cargo', choices=[
+    ('Garçom', 'Garçom'),
+    ('Balconista', 'Balconista'),
+    ('Cozinheiro', 'Cozinheiro'),
+    ('Gerente', 'Gerente')], 
+    validators=[DataRequired()])
+    telefone = StringField('Telefone', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    senha = PasswordField(label='Senha', validators=[DataRequired(), Length(min=4)])
+    imagem = FileField('Foto do Funcionário', validators=[
+        FileAllowed(['jpg', 'png', 'jpeg'], 'Somente imagens JPG e PNG!')
+    ])
+    enviar = SubmitField('Cadastrar')
+
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), Email()])
