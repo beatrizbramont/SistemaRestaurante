cdf3a07720ea49360d0d70fceed42a055772b8cf
Wender1903 <wenderman74@gmail.com>
2025-10-30 06:53:03 -0300

    :sparkles: feat: ADicionando funcionalidade de post com forms em funcionário (agr podemos criar no postman e no forms)

--- Funcionario/funcionario_routes.py
+++ Funcionario/funcionario_routes.py
@@ -1,18 +1,21 @@
-from flask import Blueprint, request, jsonify, redirect, url_for, render_template
+import os
+import re
+from flask import Blueprint, request, jsonify, redirect, url_for, render_template, current_app, flash
 from .funcionario_service  import cadastrar_funcionario, listar_usuario_email
 from .funcionario_model  import Funcionario
-from .funcionario_forms  import LoginForm
+from .funcionario_forms  import CadastroFuncionarioForm, LoginForm
 
 funcionarios_bp = Blueprint('funcionarios', __name__)   
 
 
-@funcionarios_bp.route("/funcionarios",  methods=['POST'])
-def criar_funcionario():
+@funcionarios_bp.route("/funcionarios/json",  methods=['POST'])
+def criar_funcionario_json():
     dados = request.json
 
     funcionario = Funcionario(
         nome=dados['nome'],
         cargo=dados['cargo'],
+        telefone=dados['telefone'],
         email=dados['email'],
         senha=dados['senha']
     )
@@ -23,15 +26,49 @@ def criar_funcionario():
         'id': funcionario_criado.id,
         'nome': funcionario_criado.nome,
         'cargo': funcionario_criado.cargo,
+        'telefone': funcionario_criado.telefone,
         'email': funcionario_criado.email
     }), 201
 
 
 @funcionarios_bp.route('/funcionarios', methods=['GET'])
 def funcionarios_page():
+    form_cadastro = CadastroFuncionarioForm()
     funcionarios = Funcionario.query.all()
 
-    return render_template('funcionarios.html', funcionarios=funcionarios)
+    return render_template('funcionarios.html', funcionarios=funcionarios, form_cadastro=form_cadastro)
+
+@funcionarios_bp.route('/funcionarios', methods=['POST'])
+def criar_funcionario_form():
+    form = CadastroFuncionarioForm()
+    if form.validate_on_submit():
+        nome = form.nome.data
+        cargo = form.cargo.data
+        email = form.email.data
+        senha = form.senha.data
+        telefone = re.sub(r'\D', '', form.telefone.data or '')
+
+        imagem = form.imagem.data
+        if imagem:
+            from werkzeug.utils import secure_filename
+            nome_arquivo = secure_filename(imagem.filename)
+            caminho = os.path.join(current_app.config['UPLOAD_FOLDER'], nome_arquivo)
+            imagem.save(caminho)
+        else:
+            nome_arquivo = None
+
+        funcionario = Funcionario(
+            nome=nome,
+            cargo=cargo,
+            email=email,
+            senha=senha,
+            telefone=telefone,
+            imagem=nome_arquivo
+        )
+        cadastrar_funcionario(funcionario)
+        flash(f"Funcionário {nome} cadastrado com sucesso!", "success")
+    print(form.errors)
+    return redirect(url_for('funcionarios.funcionarios_page'))
 
 
 @funcionarios_bp.route("/login", methods=["GET", "POST"])
