bcbe590b879348d1cc7d0f07bddf0200d401485c
beatrizbramont <beatriz.bramont@gmail.com>
2025-10-30 17:53:17 -0300

    Fix: atualizando backend

--- cliente/backend/app_cliente.py
+++ cliente/backend/app_cliente.py
@@ -2,11 +2,9 @@ from config import app, db
 '''Blueprint - rotas'''
 from index_route import frontend_bp
 from QtdMesas.quantidadeMesasRoutes import quantidade_bp
-from Resultado.esperaRoutes import espera_bp
 
 app.register_blueprint(frontend_bp)
 app.register_blueprint(quantidade_bp)
-app.register_blueprint(espera_bp)
 
 with app.app_context():
     db.create_all()
