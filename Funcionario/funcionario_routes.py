93e38b86a3c08fa1345f4daf22bf4bde51376389
Wender1903 <wenderman74@gmail.com>
2025-11-03 08:20:27 -0300

    :sparkles: feat: Adicionando Rota delete

--- Funcionario/funcionario_routes.py
+++ Funcionario/funcionario_routes.py
@@ -1,9 +1,9 @@
 import os
 import re
 from flask import Blueprint, request, jsonify, redirect, url_for, render_template, current_app, flash
-from .funcionario_service  import cadastrar_funcionario, listar_usuario_email
+from .funcionario_service  import cadastrar_funcionario, listar_usuario_email, verificar_chave, listar_funcionario_id, deletar_funcionario
 from .funcionario_model  import Funcionario
-from .funcionario_forms  import CadastroFuncionarioForm, LoginForm
+from .funcionario_forms  import CadastroFuncionarioForm, LoginForm, DeleteForm
 
 funcionarios_bp = Blueprint('funcionarios', __name__)   
 
@@ -35,8 +35,9 @@ def criar_funcionario_json():
 def funcionarios_page():
     form_cadastro = CadastroFuncionarioForm()
     funcionarios = Funcionario.query.all()
+    form_deletar = DeleteForm()
 
-    return render_template('funcionarios.html', funcionarios=funcionarios, form_cadastro=form_cadastro)
+    return render_template('funcionarios.html', funcionarios=funcionarios, form_cadastro=form_cadastro, form_deletar=form_deletar)
 
 @funcionarios_bp.route('/funcionarios', methods=['POST'])
 def criar_funcionario_form():
@@ -71,21 +72,42 @@ def criar_funcionario_form():
     return redirect(url_for('funcionarios.funcionarios_page'))
 
 
+@funcionarios_bp.route('/funcionarios/delete/<int:id>', methods=['POST'])
+def deletar_funcionario_route(id):
+    form = DeleteForm()
+    funcionario = listar_funcionario_id(id)
+
+    if not funcionario:
+        flash("Funcionário não encontrado.", "error")
+        return redirect(url_for('funcionarios.funcionarios_page'))
+
+    if form.validate_on_submit():
+        chave = form.chave.data
+        if verificar_chave(chave):
+            deletar_funcionario(funcionario)
+            flash(f"Funcionário {funcionario.nome} deletado com sucesso!", "success")
+        else:
+            flash("Chave incorreta. Não foi possível deletar.", "error")
+    else:
+        flash("Formulário inválido.", "error")
+
+    return redirect(url_for('funcionarios.funcionarios_page'))
+
+            
 @funcionarios_bp.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == 'POST':
 
-        email = request.form['email']
-        senha = request.form['senha']
+    if form.validate_on_submit():
+        email = form.email.data
+        senha = form.senha.data
 
         funcionario_bd = listar_usuario_email(email)
 
         if funcionario_bd and funcionario_bd.senha == senha:
             return redirect(url_for('index.index'))
-
         else:
             return jsonify({"erro": "Email ou senha inválidos"}), 401
-        
+
     return render_template("login.html", form=form)
 
