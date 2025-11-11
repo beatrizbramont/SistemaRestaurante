0497dcca5a2516beeb2bfa30d885ba7e31d5a42f
Wender1903 <wenderman74@gmail.com>
2025-11-11 16:39:17 -0300

    :sparkles: :recycle:  feat/refactor: Atualizando Rotas post e put de Cardápio para enviar dados de imagem para o Model; Refatorando maneira que essas rotas irão receber/ler os campos das requisições, hora antes recebiam/liam por dados do tipo json, agora pois, leem por forms

--- Cardapio/cardapio_routes.py
+++ Cardapio/cardapio_routes.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, request, jsonify, render_template, url_for
+import os
+from flask import Blueprint, request, jsonify, render_template, url_for, current_app
 from Cardapio.cardapio_model import db, Cardapio
+from werkzeug.utils import secure_filename
+
 
 cardapio_bp = Blueprint("cardapio", __name__)
 
@@ -11,20 +14,36 @@ def cardapio_page():
 @cardapio_bp.route("/cardapio", methods=["POST"])
 def create_item():
     try:
-        data = request.json
+        
+        nome = request.form["nome"]
+        ingredientes = request.form["ingredientes"]
+        preco = request.form["preco"]
+        categoria = request.form["categoria"]
+        tempo_preparo = request.form["tempo_preparo"]
+
+
+        imagem = request.files.get("imagem")
+        nome_arquivo = None
+
+        if imagem:
+            nome_arquivo = secure_filename(imagem.filename)
+            caminho = os.path.join(current_app.config['UPLOAD_FOLDER'], nome_arquivo)
+            imagem.save(caminho)
 
         novo_item = Cardapio(
-            nome=data["nome"],
-            ingredientes=data["ingredientes"],
-            preco=data["preco"],
-            categoria=data["categoria"],
-            tempo_preparo=data["tempo_preparo"]
+            nome=nome,
+            ingredientes=ingredientes,
+            preco=preco,
+            categoria=categoria,
+            tempo_preparo=tempo_preparo,
+            imagem=nome_arquivo
         )
+
         db.session.add(novo_item)
         db.session.commit()
 
-        items = Cardapio.query.all()
         return jsonify({"msg": "Item adicionado com sucesso!"}), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -49,11 +68,11 @@ def get_item():
 @cardapio_bp.route("/cardapio/<int:item_id>", methods=["PUT"])
 def update_item(item_id):
     try:
-        dados = request.json
         item = Cardapio.query.get(item_id)
-
         if not item:
             return jsonify({"msg": "Item não encontrado"}), 404
+
+        dados = request.form
 
         item.nome = dados.get("nome", item.nome)
         item.ingredientes = dados.get("ingredientes", item.ingredientes)
@@ -61,11 +80,32 @@ def update_item(item_id):
         item.categoria = dados.get("categoria", item.categoria)
         item.tempo_preparo = dados.get("tempo_preparo", item.tempo_preparo)
 
-        db.session.commit()
+        
+        if "imagem" in request.files:
+            imagem = request.files["imagem"]
+            if imagem:
 
+                
+                if item.imagem:
+                    caminho_antigo = os.path.join(current_app.config["UPLOAD_FOLDER"], item.imagem)
+                    if os.path.exists(caminho_antigo):
+                        os.remove(caminho_antigo)
+
+                
+                from werkzeug.utils import secure_filename
+                nome_arquivo = secure_filename(imagem.filename)
+                caminho_novo = os.path.join(current_app.config["UPLOAD_FOLDER"], nome_arquivo)
+                imagem.save(caminho_novo)
+
+                
+                item.imagem = nome_arquivo
+
+        db.session.commit()
         return jsonify({"msg": "Item atualizado com sucesso!"}), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
     
 #Delete
 @cardapio_bp.route('/cardapio/<int:item_id>', methods=['DELETE'])
