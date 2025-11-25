7253d0fa5afbc4c3d2141d405924037e6f3765a6
Wender1903 <wenderman74@gmail.com>
2025-11-24 21:04:24 -0300

    :sparkles: feat: Cria serviço para atualizar funcionario

--- Funcionario/funcionario_service.py
+++ Funcionario/funcionario_service.py
@@ -1,6 +1,8 @@
 import os
+import re
 from flask import current_app
 from config import db
+from werkzeug.utils import secure_filename
 from Funcionario.funcionario_model import Funcionario
 
 def cadastrar_funcionario(funcionario):
@@ -28,6 +30,37 @@ def verificar_chave(chave):
     if chave == '123456789':
         return True
     
+
+def atualizar_funcionario(funcionario, dados, arquivo_imagem):
+
+    nome = dados.get("nome")
+    cargo = dados.get("cargo")
+    email = dados.get("email")
+    senha = dados.get("senha")
+    telefone = re.sub(r"\D", "", dados.get("telefone", "")) 
+
+    if nome:
+        funcionario.nome = nome
+    if cargo:
+        funcionario.cargo = cargo
+    if email:
+        funcionario.email = email
+    if senha:
+        funcionario.senha = senha
+    if telefone:
+        funcionario.telefone = telefone
+
+    if arquivo_imagem:
+        nome_arquivo = secure_filename(arquivo_imagem.filename)
+        caminho = os.path.join(current_app.config['UPLOAD_FOLDER'], nome_arquivo)
+        arquivo_imagem.save(caminho)
+
+        funcionario.imagem = nome_arquivo  
+
+    db.session.commit()
+
+    return funcionario
+
 def deletar_funcionario(funcionario):
     if funcionario.imagem: 
         caminho_imagem = os.path.join(current_app.config['UPLOAD_FOLDER'], funcionario.imagem)
