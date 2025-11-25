303c776befb9c35c0e27b3c3e20b8581279af0e0
Wender1903 <wenderman74@gmail.com>
2025-11-24 21:01:47 -0300

    :sparkles: feat: Adiciona método to_dict em funcionarios model

--- Funcionario/funcionario_model.py
+++ Funcionario/funcionario_model.py
@@ -1,7 +1,7 @@
 from config import db
 
 class Funcionario(db.Model):
-    __tablename__ = 'funcionarios'
+    _tablename_ = 'funcionarios'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(150), nullable=False)
     cargo = db.Column(db.String(50), nullable=False)
@@ -10,3 +10,14 @@ class Funcionario(db.Model):
     senha = db.Column(db.String(150), nullable=False)
     imagem = db.Column(db.String(255))
 
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "cargo": self.cargo,
+            "telefone": self.telefone,
+            "email": self.email,
+            "senha": self.senha,
+            "imagem": self.imagem
+}
