18c6a6b31428ae799e109fd9342699be3c8d406e
beatrizbramont <beatriz.bramont@gmail.com>
2026-03-09 18:08:06 -0300

    feat: criando banco de dados no banco online com postgre

--- cliente/backend/config.py
+++ cliente/backend/config.py
@@ -9,22 +9,19 @@ load_dotenv()
 
 app = Flask(__name__)
 
-db_user = os.getenv("DB_USER")
-db_pass = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST")
-db_name = os.getenv("DB_NAME")
-db_pass_encoded = quote_plus(db_pass)
+app.config['SECRET_KEY'] = 'w3nd3r_t3st3_123!@#'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 app.config['HOST'] = '0.0.0.0'
 app.config['PORT'] = 8002
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{db_user}:{db_pass_encoded}@{db_host}/{db_name}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "chave_super_secreta")  
-app.config["JWT_TOKEN_LOCATION"] = ["headers"]  
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 3600  
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
